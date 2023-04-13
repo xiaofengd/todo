924b0b32b827ffd60a3653714b468a789c558d7b
xiaofengd <35124462+xiaofengd@users.noreply.github.com>
2023-04-13 13:43:01 +0800

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -13,6 +13,11 @@ root.render(
 let i = 0;
 let i;
 
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
